898eb89c0afcb4d702df0ff71c64d1ed5a7533e9
mcausse <homz70@gmail.com>
2025-08-25 22:54:50 +0200

    .

---
+++ martung-utils/src/main/java/io/exception/ExceptionUtils.java
@@ -0,0 +1,14 @@
+package io.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class ExceptionUtils {
+
+    public static String toString(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
+        return sw.toString();
+    }
+}
